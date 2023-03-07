a6c2acefb9705943004639bf4554f92bc33316ea
肖仕佳 <464736505@qq.com>
2023-03-07 17:09:52 +0800

    fit: 填充登录逻辑， [start]-[end]的代码实现见https://go-zero.dev/cn/docs/advance/jwt/

--- service/user/api/internal/logic/loginlogic.go
+++ service/user/api/internal/logic/loginlogic.go
@@ -2,9 +2,13 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"strings"
+	"time"
 
 	"go-zero-book/service/user/api/internal/svc"
 	"go-zero-book/service/user/api/internal/types"
+	"go-zero-book/service/user/model"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -25,6 +29,38 @@ func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic 
 
 func (l *LoginLogic) Login(req *types.LoginReq) (resp *types.LoginReply, err error) {
 	// todo: add your logic here and delete this line
+	if len(strings.TrimSpace(req.Username)) == 0 || len(strings.TrimSpace(req.Password)) == 0 {
+		return nil, errors.New("参数错误")
+	}
 
-	return
+	userInfo, err := l.svcCtx.UserModel.FindOneByNumber(l.ctx, req.Username)
+	switch err {
+	case nil:
+	case model.ErrNotFound:
+		return nil, errors.New("用户名不存在")
+	default:
+		return nil, err
+	}
+
+	if userInfo.Password != req.Password {
+		return nil, errors.New("用户密码不正确")
+	}
+
+	// ---start---
+	now := time.Now().Unix()
+	accessExpire := l.svcCtx.Config.Auth.AccessExpire
+	jwtToken, err := l.getJwtToken(l.svcCtx.Config.Auth.AccessSecret, now, l.svcCtx.Config.Auth.AccessExpire, userInfo.Id)
+	if err != nil {
+		return nil, err
+	}
+	// ---end---
+
+	return &types.LoginReply{
+		Id:           userInfo.Id,
+		Name:         userInfo.Name,
+		Gender:       userInfo.Gender,
+		AccessToken:  jwtToken,
+		AccessExpire: now + accessExpire,
+		RefreshAfter: now + accessExpire/2,
+	}, nil
 }
