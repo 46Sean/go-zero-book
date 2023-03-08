ba623160e4dff51a213456ffae827deb29137e3a
肖仕佳 <464736505@qq.com>
2023-03-08 15:37:09 +0800

    fit: 日志模块配置

--- service/search/api/internal/logic/searchlogic.go
+++ service/search/api/internal/logic/searchlogic.go
@@ -25,6 +25,6 @@ func NewSearchLogic(ctx context.Context, svcCtx *svc.ServiceContext) *SearchLogi
 
 func (l *SearchLogic) Search(req *types.SearchReq) (resp *types.SearchReply, err error) {
 	// todo: add your logic here and delete this line
-
-	return
+	logx.Infof("userId: %v", l.ctx.Value("userId")) // 这里的key和生成jwt token时传入的key一致
+	return &types.SearchReply{}, nil
 }
