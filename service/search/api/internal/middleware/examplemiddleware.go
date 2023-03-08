10bd2bcf50f4f6db70ae02af0af78d21d652a022
肖仕佳 <464736505@qq.com>
2023-03-08 16:59:56 +0800

    fit: 中间件完善资源依赖，log一行文字，如果日志里出现了则代表中间件使用起来了

--- service/search/api/internal/middleware/examplemiddleware.go
+++ service/search/api/internal/middleware/examplemiddleware.go
@@ -1,6 +1,9 @@
 package middleware
 
-import "net/http"
+import (
+	"github.com/zeromicro/go-zero/core/logx"
+	"net/http"
+)
 
 type ExampleMiddleware struct {
 }
@@ -10,6 +13,7 @@ func NewExampleMiddleware() *ExampleMiddleware {
 }
 
 func (m *ExampleMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
+	logx.Info("example middle")
 	return func(w http.ResponseWriter, r *http.Request) {
 		// TODO generate middleware implement function, delete after code implementation
 
