4ee148335270b9713aeb682d6db7f320b8fc0bd8
肖仕佳 <464736505@qq.com>
2023-03-09 11:39:01 +0800

    fit: 在search服务中调用user rpc；添加依赖

--- service/search/api/internal/svc/servicecontext.go
+++ service/search/api/internal/svc/servicecontext.go
@@ -2,18 +2,22 @@ package svc
 
 import (
 	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
 	"go-zero-book/service/search/api/internal/config"
 	"go-zero-book/service/search/api/internal/middleware"
+	user "go-zero-book/service/user/rpc/userclient"
 )
 
 type ServiceContext struct {
 	Config  config.Config
 	Example rest.Middleware
+	UserRpc	user.User
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config:  c,
 		Example: middleware.NewExampleMiddleware().Handle,
+		UserRpc: user.NewUser(zrpc.MustNewClient(c.UserRpc)),
 	}
 }
