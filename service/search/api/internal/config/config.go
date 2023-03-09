e17fa07452e11dbb3491171bdf5f3cd9749e7feb
肖仕佳 <464736505@qq.com>
2023-03-09 11:10:57 +0800

    fit: 在search服务中调用user rpc；添加UserRpc配置

--- service/search/api/internal/config/config.go
+++ service/search/api/internal/config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type Config struct {
 	rest.RestConf
@@ -8,6 +11,7 @@ type Config struct {
 		AccessSecret string
 		AccessExpire int64
 	}
+	UserRpc zrpc.RpcClientConf
 }
 
 type LogConf struct {
